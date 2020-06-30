5459e854f339184cda80a469a9e7b4b01768208d
AltMirrorBell <410762240@qq.com>
2020-06-30 16:47:34 +0800

    Create script.js
    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         restore github layout
+// @version      1.0
+// @author       AltMirrorBell
+// @description  what the fuck is this update
+// @match        *://github.com/*
+// @inject-into content
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.querySelector(".repohead h1").style.left = "21.5%";
+    document.querySelector(".fork-flag").style.marginLeft = "350px";
+    document.querySelector(".UnderlineNav").style.margin = "0 auto";
+    document.querySelector(".UnderlineNav").style.width = "66.5%";
+    document.querySelector(".pagehead-actions").style.right = "15.4%";
+    document.querySelector("li.d-flex").style.visibility = "visible";
+    document.querySelector(".js-responsive-underlinenav-overflow").style.visibility = "hidden";
+})();
